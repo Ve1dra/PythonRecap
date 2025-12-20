ae875f262f49bb35295b18e93c7043f13cf67b7a
Ve1dra <abdulrasheedyahya866@gmail.com>
2025-12-20 10:12:14 +0100

    New

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -2,11 +2,12 @@ from openpyxl import load_workbook
 
 def calender():pass
 
-file = "schedule_template_file.xlsx"
+file = "scheduleData.xlsx"
 wb = load_workbook(filename=file)
 sheet = wb.active
 sheet['A1'] = 'New Value'
 sheet.cell(row=2, column=2, value='Another Value')
 new_roww_data = [1,2,3,4]
 sheet.append(new_roww_data)
-wb.save("schdule_template_file.xlsx")
+wb.save("scheduleData.xlsx")
+wb.close()
